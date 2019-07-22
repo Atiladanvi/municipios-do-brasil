65f164c6a10356b6cd0081d10679702ff51a2576
Atiladanvi <atila.danvi@outlook.com>
2019-07-22 18:26:28 -0300

    Init project

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports = {
+  lintOnSave     : false,
+  runtimeCompiler: true,
+  publicPath     : './',
+  outputDir      : path.resolve(__dirname, './dist'),
+}
