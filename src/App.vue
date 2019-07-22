7fe27153837f9d7e75c0f8f64431fa8cdf9520bb
Atiladanvi <atila.danvi@outlook.com>
2019-07-22 18:31:15 -0300

    Include components into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <div class="container">
+      <b-row class="mt-3">
+        <b-col>
+          <panel-location />
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 <script>
+import PanelLocation from './components/PanelLocations'
 
-export default { name: 'App' }
+export default {
+  name      : 'App',
+  components: { PanelLocation },
+}
 </script>
